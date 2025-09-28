2102c01197ae663a63cf8f2b6b8850d5a394e9a5
Jun Hao <zhoujun.hao@outlook.com>
2025-09-28 21:19:37 +0200

    lab3 feature augmentation

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -6,10 +6,9 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    augmented=np.ones((x.shape[0],1))
+    x=x.reshape((x.shape[0],1))
+    for d in range(1,degree+1):
+        augmented=np.concatenate([augmented,x**d],1)
+    
+    return augmented
